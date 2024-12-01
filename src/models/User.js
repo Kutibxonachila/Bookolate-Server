01b4d22b2ec8cfc6d367c8e21b222da0b537cdd1
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-01 13:27:04 +0500

    User activity model

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.config.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUIDV4(),
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "Male",
+      validate: {
+        isIn: ["Male", "Female"],
+      },
+    },
+    total_borrowed_books: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    on_time_returns: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    overdue_returns: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+
+export default User
