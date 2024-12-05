42a3a3c4fd1715ec06b6065faec0ce8857077f60
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-05 23:28:09 +0500

    update all models , add AllTimePopular Book model

---
+++ src/models/AllTimePopular.Book.js
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.config.js";
+
+const AllTimePopular_Book = sequelize.define(
+  "all_time_popular_books",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    book_id: {
+      type: DataTypes.UUIDV4(),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    total_borrowed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
+);
--- src/models/User.Activity.js
+++ src/models/User.Activity.js
@@ -1,50 +1,54 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.config.js";
 
-const UserActivity = sequelize.define("borrowing_activity", {
-  id: {
-    type: DataTypes.UUIDV4(),
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.UUIDV4(),
-    allowNull: false,
-  },
-  book_id: {
-    type: DataTypes.UUIDV4,
-    allowNull: false,
-  },
-  borrow_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  due_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  return_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.VIRTUAL,
-    get() {
-      const returnDate = this.getDataValue("return_date");
-      const dueDate = this.getDataValue("due_date");
-      if (!returnDate) return "Not Returned";
-      return returnDate <= dueDate ? "On Time" : "Overdue";
+const UserActivity = sequelize.define(
+  "borrowing_activity",
+  {
+    id: {
+      type: DataTypes.UUIDV4(),
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.UUIDV4(),
+      allowNull: false,
+    },
+    book_id: {
+      type: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    borrow_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    due_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    return_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        const returnDate = this.getDataValue("return_date");
+        const dueDate = this.getDataValue("due_date");
+        if (!returnDate) return "Not Returned";
+        return returnDate <= dueDate ? "On Time" : "Overdue";
+      },
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
     },
   },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+  { timestamps: false }
+);
 
 export default UserActivity;
