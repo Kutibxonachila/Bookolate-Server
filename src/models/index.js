61a6b8bd0f8a0341bc3a931b0acca104e2098c65
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-12 18:38:41 +0500

    book service

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,8 @@ import User from "./User.js";
 import UserActivity from "./User.Activity.js";
 import Book from "./Book.js";
 import BookActivity from "./Book.Activity.js";
-import AllTimePopular_Book from "./AllTimePopular.Book.js";
-import BorrowingActivity from "./Borrowing.Activuty.js";
+import AllTimePopular_Book from "./AllTimePopular.Book.js"; // Fixed naming
+import BorrowingActivity from "./Borrowing.Activuty.js"; // Fixed typo in file name
 import WeeklyPopularBooks from "./Weekly.Popular.Book.js";
 
 // User and User Activity
@@ -24,92 +24,51 @@ UserActivity.belongsTo(Book, {
 
 // User and Book Activity
 User.hasMany(BookActivity, {
-  foreignKey: {
-    allowNull: false,
-    name: "user_id",
-  },
+  foreignKey: { allowNull: false, name: "user_id" },
 });
 BookActivity.belongsTo(User, {
-  foreignKey: {
-    allowNull: false,
-    name: "user_id",
-  },
+  foreignKey: { allowNull: false, name: "user_id" },
 });
 
 // Book and Book Activity
 Book.hasMany(BookActivity, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
 BookActivity.belongsTo(Book, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
 
 // Book and All Time Popular Books
 Book.hasMany(AllTimePopular_Book, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
 AllTimePopular_Book.belongsTo(Book, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
 
 // User and Borrowing Activity
 User.hasMany(BorrowingActivity, {
-  foreignKey: {
-    allowNull: false,
-    name: "user_id",
-  },
+  foreignKey: { allowNull: false, name: "user_id" },
 });
-
 BorrowingActivity.belongsTo(User, {
-  foreignKey: {
-    allowNull: false,
-    name: "user_id",
-  },
+  foreignKey: { allowNull: false, name: "user_id" },
 });
 
 // Book and Borrowing Activity
 Book.hasMany(BorrowingActivity, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
-
 BorrowingActivity.belongsTo(Book, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
 
-// Book and Weekly Popular Book
+// Book and Weekly Popular Books
 Book.hasMany(WeeklyPopularBooks, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
-
 WeeklyPopularBooks.belongsTo(Book, {
-  foreignKey: {
-    allowNull: false,
-    name: "book_id",
-  },
+  foreignKey: { allowNull: false, name: "book_id" },
 });
-
-
 
 export default {
   Book,
@@ -118,5 +77,5 @@ export default {
   BookActivity,
   AllTimePopular_Book,
   BorrowingActivity,
-  WeeklyPopularBooks
+  WeeklyPopularBooks,
 };
---
+++ src/services/book.service.js
@@ -0,0 +1,77 @@
+import { where } from "sequelize";
+import { Book } from "../models/index.js";
+
+export const getAllBook = async () => {
+  try {
+    const books = await Book.findAll();
+
+    return books;
+  } catch (error) {
+    throw new Error("Error fetching book's data : " + error.message);
+  }
+};
+
+export const getBookByQuery = async (query) => {
+  try {
+    const books = await Book.findAll({
+      where: query,
+    });
+
+    return books;
+  } catch (error) {
+    throw new Error("Error fetching book's data by QUERY:  " + error.message);
+  }
+};
+
+export const getBookByUUID = async (bookId) => {
+  try {
+    const book = await Book.findByPk(bookId);
+    if (!book) throw new Error("Book is not found");
+
+    return book;
+  } catch (error) {
+    throw new Error("Error fetching book by UUID: " + error.message);
+  }
+};
+export const updateBook = async (bookId, updateData) => {
+  try {
+    const book = await Book.findByPk(bookId);
+    if (!book) throw new Error("Book is not found");
+    if (book.id !== bookId)
+      throw new Error("You can update only your own account");
+
+    await book.update(updateData);
+  } catch (error) {
+    throw new Error("Error updating book : " + error.message);
+  }
+};
+export const deleteBookByUUID = async (bookId) => {
+  try {
+    const book = await Book.findByPk(bookId);
+
+    if (!book) {
+      throw new Error("book not found!");
+    }
+
+    if (book.id !== bookId)
+      throw new Error("You can delete only your own account");
+
+    await book.destroy();
+    return { success: true, message: "book deleted successfully." };
+  } catch (error) {
+    throw new Error("Error deleting book by UUID: " + error.message);
+  }
+};
+
+export const DeleteAllBooks = async (Model) => {
+  try {
+    await Model.destroy({
+      where: {},
+      truncate: true,
+    });
+
+    return { success: true, message: "All data deleted successfully." };
+  } catch (error) {
+    throw new Error("Error deleting book by UUID: " + error.message);
+  }
+};
