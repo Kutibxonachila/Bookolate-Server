326801f021b8b485546738f25d08b9b165d6606e
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-13 14:27:31 +0500

    add logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,13 +1,31 @@
-import winston, { loggers } from "winston";
+import winston, { error } from "winston";
 
+//COnfigure logger
 const logger = winston.createLogger({
-  level: "debug",
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.timestamp(), //add timestamps
+    winston.format.printf(
+      ({ level, message, timestamp }) =>
+        `${timestamp}  [${level.toLocaleUpperCase()}] :  ${message}`
+    )
+  ),
   transports: [
-    new winston.transports.Console({ format: winston.format.simple() }),
+    new winston.transports.Console({
+      format: winston.format.colorize({ all: true }), // Colorize console logs
+    }),
+    new winston.transports.File({
+      filename: "../logs/error.log",
+      level: error,
+    }), // Error logs
+    new winston.transports.File({
+      filename: "../logs/combined.log",
+    }), // All logs
   ],
 });
 
-loggers.add("default", logger);
-logger.info("This is info message");
+logger.debug("This is a debug message");
+logger.info("This is an info message");
+logger.error("This is an error message");
 
 export default logger;
