3528b529be6c786c0d910561a50e7035a8fe0494
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-20 00:28:19 +0500

    add weekly.popular.book.router.js

--- src/routes/weekly.popular.book.router.js
+++ src/routes/weekly.popular.book.router.js
@@ -1,1 +1,15 @@
- 
+import express from "express";
+import {
+  fetchWeeklyPopularBooks,
+  incrementWeeklyPopularity,
+} from "../controllers/weekly.popular.book.controller.js";
+
+const router = express.Router();
+
+// Route to fetch all weekly popular books
+router.get("/weekly_books", fetchWeeklyPopularBooks);
+
+// Route to increment the weekly popularity of a specific book
+router.patch("/weekly_book/:bookId", incrementWeeklyPopularity);
+
+export default router;
