5f84dfa29a1a1d33242024828947704a22ae6d21
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-19 11:17:00 +0500

    add user.activity.router.js

--- src/routes/user.activity.router.js
+++ src/routes/user.activity.router.js
@@ -1,1 +1,15 @@
- 
+ import express from "express";
+ import {
+   borrowBookController,
+   returnBookController,
+ } from "../controllers/user.activity.controller.js";
+
+ const routerUserBorrow = express.Router();
+
+ // Route for borrowing a book
+ router.post("/borrow", borrowBookController);
+
+ // Route for returning a book
+ routerUserBorrow.post("/return", returnBookController);
+
+ export default routerUserBorrow;
