be5f1a16bb469558d929caf48602d128ddb935c9
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-20 00:24:56 +0500

    add user.router.js

--- src/routes/user.activity.router.js
+++ src/routes/user.activity.router.js
@@ -1,15 +1,15 @@
- import express from "express";
- import {
-   borrowBookController,
-   returnBookController,
- } from "../controllers/user.activity.controller.js";
+import express from "express";
+import {
+  borrowBookController,
+  returnBookController,
+} from "../controllers/user.activity.controller.js";
 
- const routerUserBorrow = express.Router();
+const routerUserBorrow = express.Router();
 
- // Route for borrowing a book
- router.post("/borrow", borrowBookController);
+// Route for borrowing a book
+router.post("/borrow", borrowBookController);
 
- // Route for returning a book
- routerUserBorrow.post("/return", returnBookController);
+// Route for returning a book
+routerUserBorrow.post("/return", returnBookController);
 
- export default routerUserBorrow;
+export default routerUserBorrow;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,1 +1,31 @@
- 
+ import express from "express";
+import {
+  fetchAllUsers,
+  fetchUserByQuery,
+  fetchUserByUUID,
+  modifyUser,
+  removeUserByUUID,
+  removeAllUsers,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+// Get all users
+router.get("/users", fetchAllUsers);
+
+// Get user by query (e.g., ?name=John)
+router.get("/users/query", fetchUserByQuery);
+
+// Get user by UUID
+router.get("/users/:id", fetchUserByUUID);
+
+// Update user
+router.put("/users/:id", modifyUser);
+
+// Delete user by UUID
+router.delete("/users/:id", removeUserByUUID);
+
+// Delete all users
+router.delete("/users", removeAllUsers);
+
+export default router;
