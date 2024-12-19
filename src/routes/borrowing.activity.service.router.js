28019bce1451024109dcb439f91f3db1a5aaa491
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-19 11:12:22 +0500

    add borrowing.activity.service.router.js

--- src/routes/borrowing.activity.service.router.js
+++ src/routes/borrowing.activity.service.router.js
@@ -1,1 +1,15 @@
- 
+import express from "express";
+import {
+  borrowBookController,
+  returnBookController,
+} from "../controllers/borrowing.activity.service.controlller.js"; // Adjust the path as needed
+
+const routerBorrow = express.Router();
+
+// Route for borrowing a book
+routerBorrow.post("/borrow", borrowBookController);
+
+// Route for returning a book
+routerBorrow.post("/return", returnBookController);
+
+export default routerBorrow;
