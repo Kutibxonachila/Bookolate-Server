f5ef2d3292284935b41c265353ca1eaa98520963
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-28 08:49:29 +0500

    added convertter from string to number

---
+++ src/utils/convertNumber.js
@@ -0,0 +1,12 @@
+function convertToNumber(inputString) {
+    // Try to convert the string to a number
+    const number = Number(inputString);
+
+    // Check if it's a valid number
+    if (isNaN(number)) {
+        return "Error: The input is not a valid number.";
+    }
+    return number;
+}
+
+export default conv
