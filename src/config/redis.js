768d0d61212ee60c5f935fa2879ce30dae3557fa
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-12-28 17:27:46 +0500

    fix X3 redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,9 @@
 import { createClient } from "redis"; // Correct import for node-redis v4+
 
-// Create Redis client with IPv4 address explicitly
-const client = createClient("rediss://default:Ac_gAAIjcDEwYjNlOTRjNTExMDY0NzUwODIzMTAyMDk5MDM0N2Q0ZXAxMA@romantic-spider-53216.upstash.io:6379");
+// Create Redis client with the URL in the correct format
+const client = createClient({
+  url: "rediss://default:Ac_gAAIjcDEwYjNlOTRjNTExMDY0NzUwODIzMTAyMDk5MDM0N2Q0ZXAxMA@romantic-spider-53216.upstash.io:6379"
+});
 
 // Log errors
 client.on("error", (err) => {
